48bbd9cfff2f33d6a30c7a1d76d1a41e68362f44
DavidOk14 <114937803+DavidOk14@users.noreply.github.com>
2024-06-07 22:17:33 -0500

    Basic Design of Login Page Complete

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
+import kivy
 from kivymd.app import MDApp
-from kivymd.uix.screen import MDScreen
+from kivy.uix.screenmanager import ScreenManager
+
+# Import Screens
+from Screens.Login.login import LoginPage
 
 kivy.require('2.3.0')
 
 class EatExperts (MDApp):
     def build(self):
+        try:
+            sm = ScreenManager()
+
+            sm.add_widget(LoginPage(name='login'))
+            sm.current = 'login'
+            return sm
+        except Exception as e:
+            print(f"{e}")
+
         pass
 
 if __name__ == "__main__":
