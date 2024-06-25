076a1fe2acd9840804f47f882593ec5f687c6437
DavidOk14 <114937803+DavidOk14@users.noreply.github.com>
2024-06-25 14:29:13 -0500

    Delete .buildozer directory

--- .buildozer/android/app/main.py
+++
@@ -1,9 +0,0 @@
-from kivymd.app import MDApp
-from kivymd.uix.screen import MDScreen
-
-class EatExperts (MDApp):
-    def build(self):
-        pass
-
-if __name__ == "__main__":
-    EatExperts().run()
